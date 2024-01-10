ddac7790f790e1f651858cdc20935e6c563d74af
Mi Berry <momoberry2@hotmail.com>
2024-01-10 15:20:58 -0500

    under 3 m

---
+++ src/leetcodeeasy/question41.java
@@ -0,0 +1,28 @@
+package leetcodeeasy;
+/*
+ * Accepted
+nimo22
+nimo22
+submitted at Jan 10, 2024 15:12
+Runtime
+1ms
+Beats93.01%of users with Java
+Memory
+46.81MB
+Beats7.61%of users with Java
+ */
+public class question41 {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null){
+            return null;
+        }else{
+            if(head.val==val && head.next!=null){
+                return removeElements( head.next,  val);
+            }else if(head.val==val &&head.next==null ){
+                return null;
+            }
+        }
+
+        return new ListNode(head.val,removeElements( head.next,  val));
+    }
+}
